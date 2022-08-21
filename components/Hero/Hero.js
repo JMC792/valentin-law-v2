b759b6736d54410e40973d05e8130fd99193c61d
Mauricio Chavez <mauriciochavez792@gmail.com>
2022-08-21 15:56:48 -0400

    coming soon banner

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -8,10 +8,9 @@ function Hero() {
         <Fragment>
             <header className="h-screen bg-back-2 bg-cover" id="header">
                 <div className="container flex justify-center">
-                    <div className="pt-[10rem] md:pt-[15rem] lg:pt-[23rem] text-center text-neutral-200">
-                        <h1 className="text-2xl lg:text-7xl drop-shadow-xl">Bienvenidos a Encuentro Church</h1>
-                        <h3 className="pt-4 text-xl lg:text-2xl drop-shadow-xl">Conectando a todos por el amor de dios</h3>
-                        <button type="button" className="text-white bg-yellow-600 rounded-lg md:text-[20px] leading-[25px] px-7 py-4 mt-4 w-[14rem] uppercase">aprende mas</button>
+                    <div className="pt-[10rem] md:pt-[15rem] lg:pt-[23rem] text-center text-neutral-200 grid gap-y-10">
+                        <h1 className="text-3xl lg:text-7xl drop-shadow-xl">Encuentro Church</h1>
+                        <h3 className="text-xl lg:text-4xl drop-shadow-xl">Coming Soon</h3>
                     </div>
                 </div>
             </header>
--- pages/index.js
+++ pages/index.js
@@ -16,39 +16,10 @@ export default function Home() {
       </Head>
 
       <main>
-          <Navbar/>
           <Hero/>
-
-          {/* Quote */}
-          <div className='bg-orange-100'>
-            <div className='container flex justify-center py-32'>
-              <div className='text-center grid gap-y-6'>
-                <div className='md:text-2xl'>Nuestra Iglesia</div>
-                <div className="md:text-[2rem]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
-                <div>By Jesus Chavez</div>
-                <div>Lead Pastor</div>
-              </div>
-            </div>
-          </div>
-
-          {/* About */}
-          <div className='container py-32'>
-            <div className='grid md:grid-cols-2 md:p-12'>
-              <div>
-                  <img src="public/images/youth-group.jpg" alt="youth group" className="h-full"/>
-              </div>
-              <div className='grid gap-y-6'>
-                <h1 className='text-2xl'>About Us</h1>
-                <div className=" md:text-4xl">Jesus Taught Us to Love Like God Loves</div>
-                <p className="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-              </div>
-            </div>
-          </div>
-
 
       </main>
 
-      <Footer />
     </Fragment>
   )
 }
