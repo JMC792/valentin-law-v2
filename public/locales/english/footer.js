436d0831a0ffeb8f430ef865f7c32a7a8d207dbb
Mauricio Chavez <mauriciochavez792@gmail.com>
2023-03-12 17:35:49 -0400

    refactored footer

--- public/locales/english/footer.js
+++ public/locales/english/footer.js
@@ -1,22 +1,39 @@
 module.exports = {
+
+    //Overview 
     overviewTitle : "Encuentro Church",
-    overviewParagraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non tellus orci ac auctor augue mauris augue neque. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla",
-    overviewPhoneNumber : "(980)-290-7074",
-    overviewEmail : "jesus@encuentro.church",
-    overviewAddress : "6900 Democracy Dr, Charlotte, NC 28212",
+        overviewParagraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non tellus orci ac auctor augue mauris augue neque. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla",
+        overviewPhoneNumber : "(980)-290-7074",
+        overviewEmail : "jesus@encuentro.church",
+        overviewAddress : "7568 Ruben Linker Rd NW, Concord, NC 28027",
+        overviewAddressLink : "https://goo.gl/maps/8LrxauLEj5XFz5b38",
+    
+    //Useful Links
     usefulLinkTitle : "Useful Links",
-    usefulLink1 : "About",
-    usefulLink2 : "Services",
-    usefulLink3 : "Donate",
-    usefulLink4 : "Team",
+        //Link 1
+        usefulLink1 : "About",
+        usefulLink1Path : "#about",
+        //Link 2
+        usefulLink2 : "Services",
+        usefulLink2Path : "#services",
+        //Link 3
+        usefulLink3 : "Donate",
+        usefulLink3Path : "https://tithe.ly/give?c=5922800",
+        //Link 4
+        usefulLink4 : "Team",
+        usefulLink4Path : "#team",
+    
+    //Social Media
     socialMediaTitle : "Follow Our Socials",
-    socialMedia1 : "Instagram",
-    socialMedia2 : "Youtube",
-    socialMedia3 : "Facebook",
-    socialMedia4 : "Twitter",
+        socialMedia1 : "Instagram",
+        socialMedia2 : "Youtube",
+        socialMedia3 : "Facebook",
+        socialMedia4 : "Twitter",
+    
+    //Legal
     legalTitle : "Legal",
-    legalLink1 : "About",
-    legalLink2 : "Privacy Policy",
-    legalLink3 : "Licensing",
-    legalLink4 : "Contact",
+        legalLink1 : "About",
+        legalLink2 : "Privacy Policy",
+        legalLink3 : "Licensing",
+        legalLink4 : "Contact",
 }
--- public/locales/spanish/footer.js
+++ public/locales/spanish/footer.js
@@ -1,22 +1,40 @@
 module.exports = {
+    
+    //Overview 
     overviewTitle : "Encuentro Church",
-    overviewParagraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non tellus orci ac auctor augue mauris augue neque. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla",
-    overviewPhoneNumber : "(980)-290-7074",
-    overviewEmail : "jesus@encuentro.church",
-    overviewAddress : "6900 Democracy Dr, Charlotte, NC 28212",
+        overviewParagraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non tellus orci ac auctor augue mauris augue neque. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla",
+        overviewPhoneNumber : "(980)-290-7074",
+        overviewEmail : "jesus@encuentro.church",
+        //Address
+        overviewAddress : "7568 Ruben Linker Rd NW, Concord, NC 28027",
+        overviewAddressLink : "https://goo.gl/maps/8LrxauLEj5XFz5b38",
+    
+    //Usefull Links
     usefulLinkTitle : "Enlaces Utiles",
-    usefulLink1 : "De Nosotros",
-    usefulLink2 : "Sermones",
-    usefulLink3 : "Donar",
-    usefulLink4 : "Equipo",
+        //Link 1
+        usefulLink1 : "De Nosotros",
+        usefulLink1Path : "#about",
+        //Link 2
+        usefulLink2 : "Sermones",
+        usefulLink2Path : "#services",
+        //Link 3
+        usefulLink3 : "Donar",
+        usefulLink3Path : "https://tithe.ly/give?c=5922800",
+        //Link 4
+        usefulLink4 : "Equipo",
+        usefulLink4Path : "#team",
+    
+    //Social Media
     socialMediaTitle : "Nuestras Redes Sociales",
-    socialMedia1 : "Instagram",
-    socialMedia2 : "Youtube",
-    socialMedia3 : "Facebook",
-    socialMedia4 : "Twitter",
+        socialMedia1 : "Instagram",
+        socialMedia2 : "Youtube",
+        socialMedia3 : "Facebook",
+        socialMedia4 : "Twitter",
+
+    //Legal
     legalTitle : "Legal",
-    legalLink1 : "About",
-    legalLink2 : "Política de Privacidad",
-    legalLink3 : "Licencia",
-    legalLink4 : "Contacto",
+        legalLink1 : "About",
+        legalLink2 : "Política de Privacidad",
+        legalLink3 : "Licencia",
+        legalLink4 : "Contacto",
 }
